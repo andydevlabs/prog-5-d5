f6bcb186598d28c4945a14cc6ad04830ea70d0c6
andydevlabs <avotiana@mail.hei.school>
2025-05-28 09:47:07 +0300

    refactor: rearrange the code

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import promptSync from "prompt-sync";
-const prompt = promptSync({ sigint: true })
-; 
+const prompt = promptSync({ sigint: true });
+
 let isPaid = false;
 let price = 10000;
-
 const flavors = [
     {
         name: "latte",
@@ -22,46 +21,54 @@ const flavors = [
         isAvailable: false,
     },
     {
-        name: "americano",
+        name: "cappuccino",
         isAvailable: true,
     },
 ];
 
 function pay(amount) {
-    if (amount === price) {
-        isPaid = true;
-        console.log("Choose your flavor : ");
-        flavors.map((flavor) => {
-            console.log(`- ${flavor.name.toUpperCase()}`);
-        });
-        const ask = prompt("> ");
-        const selectedFlavor = flavors.find(
-            (flavor) => flavor.name.toLowerCase() === ask.toLowerCase()
-        );
-
-        if (selectedFlavor) {
-            if (selectedFlavor.isAvailable) {
-                console.log("Here's your coffee");
-            } else {
-                console.log(
-                    "We're sorry but that flavor is unavailable for the moment"
-                );
-            }
-        } else {
-            console.log("Please, make sure you enter the right choice");
-        }
-        
+    if (isNaN(amount)) {
+        console.log("Please enter a number for your payment");
+        return false;
     }
 
     if (amount < price) {
         let moneyToPay = price - amount;
         console.log(`Insufficient payment, need to pay ${moneyToPay}ar more`);
+        return false;
     }
 
     if (amount > price) {
         let restOfMoney = amount - price;
         console.log(`Don't forget your change : ${restOfMoney}ar`);
     }
+
+    if (amount === price) {
+        isPaid = true;
+        if (isPaid) {
+            console.log("Choose your flavor : ");
+            flavors.map((flavor) => {
+                console.log(`- ${flavor.name.toUpperCase()}`);
+            });
+
+            const ask = prompt("> ");
+            const selectedFlavor = flavors.find(
+                (flavor) => flavor.name.toLowerCase() === ask.toLowerCase()
+            );
+
+            if (selectedFlavor) {
+                if (selectedFlavor.isAvailable) {
+                    console.log("Here's your coffee");
+                } else {
+                    console.log(
+                        "We're sorry but that flavor is unavailable for the moment"
+                    );
+                }
+            } else {
+                console.log("Please, make sure you enter the right choice");
+            }
+        }
+    }
 }
 
-pay(10000);
+pay("zge");
