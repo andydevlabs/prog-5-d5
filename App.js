dde597095cba3350fa80cbaeb7f8db1814a39c18
andydevlabs <avotiana@mail.hei.school>
2025-05-28 08:56:14 +0300

    feat: add payement error handling

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 let isPaid = false;
-let price = 10
-const flavors = ["latte", "espresso", "americano", "mocha", "americano"]
+let price = 10000;
+const flavors = ["latte", "espresso", "americano", "mocha", "americano"];
 
 function pay(amount) {
     if (amount === price) {
-        isPaid = true
-        if (isPaid) {
-            flavors.map((flavor) => {
-                console.log(flavor);
-            });
-        }
+        isPaid = true;
+        console.log("Choose your flavor : ");
+        flavors.map((flavor) => {
+            console.log(`- ${flavor.toUpperCase()}`);
+        });
+    }
+
+    if (amount < price) {
+        let moneyToPay = price - amount
+        console.log(`Insufficient payment, need to pay ${moneyToPay}ar more`);
+    }
+
+    if (amount > price) {
+        let restOfMoney = amount - price
+        console.log(`Don't forget your change : ${restOfMoney}ar`);
     }
 }
 
-pay(10);
+pay(10000);
 
-// if client pay 
-    // choose flavor
-    // if flavor
-        // getCoffee
-        // else tell client no flavor
+// if client pay
+// choose flavor
+// if flavor
+// getCoffee
+// else tell client no flavor
